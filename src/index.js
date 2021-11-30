eb115f5fc2f466cfccb5a8366c58e196f126c7e8
Valery Berezin <ligateam2023@yandex.ru>
2021-12-01 02:28:26 +0300

    test hbs

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
+import template from './index.hbs';
 
-
+document.addEventListener('DOMContentLoaded', () => {
+    document.body.innerHTML = template({ firstname: 'John', lastname: 'Smith' });
+});
