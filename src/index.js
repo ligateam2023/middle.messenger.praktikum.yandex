01488e9db34da98425788269cbdb692991574122
Valery Berezin <ligateam2023@yandex.ru>
2021-12-09 14:02:54 +0300

    added simple routing

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import './pages/login/index.js';
-import './pages/main/index.js';
-import './pages/register/index.js';
-import './pages/settings/index.js';
 import {loginPage} from './pages/login/login';
 import {mainPage} from './pages/main/main';
 import {registerPage} from './pages/register/register';
 import {settingsPage} from './pages/settings/settings';
+import {page404} from "./pages/error/404";
+import {page500} from "./pages/error/500";
 
 document.addEventListener('DOMContentLoaded', () => {
     const pages = {
         login: loginPage,
         main: mainPage,
         register: registerPage,
-        settings: settingsPage
+        settings: settingsPage,
+        404: page404,
+        500: page500
     }
     function render(name) {
         document.body.innerHTML = pages[name]();
