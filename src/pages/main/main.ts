9c1051a84f4ea126e795fc72857a9a5440abb63c
Valery Berezin <ligateam2023@yandex.ru>
2022-02-12 19:54:11 +0300

    сделана страница чата

--- src/pages/main/main.ts
+++ src/pages/main/main.ts
@@ -1,8 +1,13 @@
 import mainPageTpl from './main.hbs';
 import * as styles from './main.css';
+import imgAvatar from '../../../static/images/avatar.svg';
+import imgDots from '../../../static/images/dots.svg';
+import imgExample from '../../../static/images/example.png';
+import imgAttach from '../../../static/images/attach.svg';
+import imgSend from '../../../static/images/send_arrow.svg';
 
 export function mainPage() {
-    const html = mainPageTpl({styles});
+    const html = mainPageTpl({styles, imgAvatar, imgDots, imgExample, imgAttach, imgSend});
 
     return html;
 }
